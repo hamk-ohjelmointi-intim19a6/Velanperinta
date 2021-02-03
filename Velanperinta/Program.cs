20be44cea3f506acd0dba37748d77c9de560e985
Kai Sinkkonen <kai.sinkkonen@gmail.com>
2021-02-03 20:54:24 +0200

    Add project files.

---
+++ Velanperinta/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Velanperinta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
